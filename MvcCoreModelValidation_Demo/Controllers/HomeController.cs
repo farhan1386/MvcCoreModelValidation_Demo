3df98fd062241fb23ab7c4b039356e7937d409fd
Farhan Ahmed <farhan1086@gmail.com>
2020-02-17 12:40:02 +0530

    initial commit

---
+++ MvcCoreModelValidation_Demo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreModelValidation_Demo.Models;
+
+namespace MvcCoreModelValidation_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+        }
+
+    }
+}
